ec69465b8e4cdd3c66a61328e2dd137798ecccad
coolsyd <jia1324@126.com>
2017-02-07 22:04:46 +0800

    #代码变更

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
 var config = {
-    //devMode 为true 时, 为开发模式
-    devMode: true,
-    // debug 为 true 时，用于本地调试
-    debug: true,
     // 程序运行的端口
     port: 3000,
     backend: {
@@ -10,16 +6,5 @@ var config = {
         port: 18080
     },
     pvdUrl: 'http://172.16.61.246:8886',
-    socketTimeout: 20 * 1000,//ms
-    theme_list: ['default', 'dark'],
-    app: {
-        title: '公安大数据'
-    }
 };
-if (!config.devMode) {
-    config.port = 'PBD_PORT';
-    config.backend.host = 'BACKEND_HOST';
-    config.backend.port = 'BACKEND_PORT';
-    config.pvdUrl = 'http://PVD_URL_IP:PVDURL_PORT';
-}
 module.exports = config;
