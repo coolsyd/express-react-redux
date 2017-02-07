24b03e17da75c65e66753c4d8443db0b4ec1b233
coolsyd <jia1324@126.com>
2017-02-07 23:10:05 +0800

    #代码变更

--- react/index.js
+++ react/index.js
@@ -1,27 +1,19 @@
 import React from 'react';
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { Router, browserHistory } from 'react-router'
-
+import {render} from 'react-dom'
+import {Provider} from 'react-redux'
+import {Router, browserHistory} from 'react-router'
 import createStore from './store'
-
 import routes from './routes'
-
-
-//import * as reducers from './reducers'
 import reducers from './reducers'
 
 const store = createStore(reducers);
-
-//middleware.listenForReplays(store)
-
 const rootElement = document.getElementById('root');
 
 render(
     <Provider store={store}>
-            <Router history={browserHistory}>
-                {routes(store.getState())}
-            </Router>
+        <Router history={browserHistory}>
+            {routes(store.getState())}
+        </Router>
     </Provider>,
     rootElement
 );
--- react/reducers/index.js
+++ react/reducers/index.js
@@ -1,13 +1,10 @@
 import { combineReducers } from 'redux'
-
-
 import login from './login'
 import qr from './qr'
 
 const reducer = combineReducers({
     login:login,
     qr:qr
-    //routing: routeReducer
 });
 
 export default reducer;
