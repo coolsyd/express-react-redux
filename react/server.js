3cccc564bbe34a201d576f7ffac86c50d075021a
coolsyd <jia1324@126.com>
2017-02-17 19:25:00 +0800

    #代码变更

--- react/server.js
+++ react/server.js
@@ -1,17 +1,21 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
+var cf=require('../config');
 
 new WebpackDevServer(webpack(config), {
-  hot: true,
-  contentBase: __dirname,
-  stats: {
-    colors: true
-  }
+    hot: true,
+    proxy: {
+        "/PBD/*": cf.PBD_interface
+    },
+    contentBase: __dirname,
+    stats: {
+        colors: true
+    }
 }).listen(3009, 'localhost', function (err) {
-  if (err) {
-    console.log(err);
-  }
+    if (err) {
+        console.log(err);
+    }
 
-  console.log('Listening at localhost:3009');
+    console.log('Listening at localhost:3009');
 });
